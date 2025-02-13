68d9abe27f1cba262d0df18f398d5928ac236954
Matthew Michilot <matthew.michilot@gmail.com>
2025-02-12 20:05:43 -0800

    Remove verilator build artifacts

--- sim/obj_dir/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /home/mmichilot/oss-cad-suite/share/verilator/include/verilated_threads.cpp \
- /home/mmichilot/oss-cad-suite/share/verilator/include/verilatedos.h \
- /home/mmichilot/oss-cad-suite/share/verilator/include/verilated_threads.h \
- /home/mmichilot/oss-cad-suite/share/verilator/include/verilated.h \
- /home/mmichilot/oss-cad-suite/share/verilator/include/verilated_config.h \
- /home/mmichilot/oss-cad-suite/share/verilator/include/verilated_types.h \
- /home/mmichilot/oss-cad-suite/share/verilator/include/verilated_funcs.h
